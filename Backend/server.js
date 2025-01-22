15d0b2ee52f7b05255d3dc52976326912c8ee402
Vikram Vicky <123199376+vikramvicky-1@users.noreply.github.com>
2025-01-22 14:22:42 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,9 @@ const port = process.env.PORT || 4000;
 
 //middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "https://fullstack-food-fontend.onrender.com", // Replace with your frontend's URL
+}));
 
 //DB connection
 connectDB();
